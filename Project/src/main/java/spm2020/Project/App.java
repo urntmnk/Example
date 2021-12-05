1583811f18ea4fa757a0e35229b425a4eb207244
Uran <uran.tamnik@studenti.unicam.it>
2021-12-05 18:18:54 +0100

    Commited project

---
+++ Project/src/main/java/spm2020/Project/App.java
@@ -0,0 +1,13 @@
+package spm2020.Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
